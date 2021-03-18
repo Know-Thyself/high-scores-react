9c4e3c9e8916847f57b87b08174a76a2302c6cf8
Biruk Kebede <biruk.key@gmail.com>
2021-03-18 03:29:26 +0000

    Completed level 3 - scores are numerically sorted in a descending order

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -4,8 +4,19 @@ const HighScoreTable = (props) => {
     const countryNames = props.country.sort((a,b)=> {
         let a1 = a.name.toLowerCase();
         let b1 = b.name.toLowerCase();
-        return a1 < b1 ? -1: a1 > b1 ? 1:0;
-    })
+        return a1 < b1 ? -1 : a1 > b1 ? 1 : 0;
+    });
+    
+    countryNames.map(highest => {
+        return highest.scores.sort((a, b) => {
+            if (a.s > b.s) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+    });
+
     return (
         <table className="table">
             {countryNames.map(header => (
@@ -18,12 +29,12 @@ const HighScoreTable = (props) => {
                                 <th>{hero.n}</th>
                                 <th>{hero.s}</th>
                             </tr>
-                        )
+                        );
                     })}
                 </thead>
             ))}
         </table>
-    )
+    );
 }
 
 export default HighScoreTable;
