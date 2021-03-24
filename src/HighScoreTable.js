fce14eb7844fb6ab15870774839d0482e3285f91
Biruk Kebede <biruk.key@gmail.com>
2021-03-24 04:40:48 +0000

    Completed level 4:- sorted scores in ascending and descending order

--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const HighScoreTable = (props) => {
     const countryNames = props.country.sort((a,b)=> {
@@ -6,34 +6,57 @@ const HighScoreTable = (props) => {
         let b1 = b.name;
         return a1 < b1 ? -1 : a1 > b1 ? 1 : 0;
     });
-    
-    countryNames.map(highest => {
+    const descendingOrder = () => { 
+        countryNames.map(highest => {
+            return highest.scores.sort((a, b) => {
+                if (a.s > b.s) {
+                  return -1;
+                } else {
+                    return 1;
+                  }
+            });
+        });
+    }
+    const [toggled, setToggled] = useState(false);
+    const toggler = (e) => {
+        setToggled(!toggled);
+        if (e.target.className !== "toggle") {
+            ascendingOrder(); 
+        } else if(e.target.className === "toggle"){
+            descendingOrder();
+        }
+              
+    }
+    const ascendingOrder = () => {countryNames.map(highest => {
         return highest.scores.sort((a, b) => {
-            if (a.s > b.s) {
+            if (a.s < b.s) {
                 return -1;
             } else {
                 return 1;
               }
         });
     });
-
+}
     return (
-        <table className="table">
-            {countryNames.map(header => (
-                <thead> 
-                    <caption className="country-headers">High Scores: {header.name}
-                    </caption>
-                    {header.scores.map((hero) => {
-                        return (
-                            <tr className="country-heros"> 
-                                <th>{hero.n}</th>
-                                <th>{hero.s}</th>
-                            </tr>
-                        );
-                    })}
-                </thead>
-            ))}
-        </table>
+        <div> 
+            <button onClick={toggler} className={toggled ? "toggle" : "toggle-button"}>Sort Scores <span>&#8597;</span></button>
+            <table className="table">
+                {countryNames.map(header => (
+                    <thead> 
+                        <caption className="country-headers">High Scores: {header.name}
+                        </caption>
+                        {header.scores.map((hero) => {
+                            return (
+                                <tr className="country-heros"> 
+                                    <th>{hero.n}</th>
+                                    <th>{hero.s}</th>
+                                </tr>
+                            );
+                        })}
+                    </thead>
+                ))}
+            </table>
+        </div>
     );
 }
 
