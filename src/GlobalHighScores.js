3417c15db909dfb52e07fb746609875a78bd96d2
Biruk Kebede <biruk.key@gmail.com>
2021-04-11 19:37:21 +0100

    improved indentation

--- src/GlobalHighScores.js
+++ src/GlobalHighScores.js
@@ -1,51 +1,52 @@
 import React from 'react';
 
 const GlobalHighScores = (props) => {
-
-    const playersAndScores = [];
-    props.global.forEach(entry => {
-        entry.scores.forEach(player => {
-            playersAndScores.push([player.n, player.s])
-        })
+  const playersAndScores = [];
+  props.global.forEach(entry => {
+    entry.scores.forEach(player => {
+      playersAndScores.push([player.n, player.s]);
     })
+  })
 
-    function arraySorter(arr) {
-        let jsonObj = {};
-        let values = [];
-        let result = [];
-        for (let i = 0; i < arr.length; i++) {
-            jsonObj[arr[i][1]] = arr[i][0];
-        }
-        values = Object.keys(jsonObj).sort((a, b) => b - a);
-
-        for (let j = 0; j < values.length; j++) {
-            result.push([jsonObj[values[j]], values[j]]);
-        }
-        return result;
+  function arraySorter(arr) {
+    let jsonObj = {};
+    let values = [];
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+      jsonObj[arr[i][1]] = arr[i][0];
     }
-    const sortedArray = arraySorter(playersAndScores);
+    values = Object.keys(jsonObj).sort((a, b) => b - a);
 
-    return (
-        <table className="g-table">
-            <thead>
-                <tr className="header">
-                    <th className="g-header" scope="col">Name</th>
-                    <th className="g-header" scope="col">Score</th>
-                </tr>
-            </thead>
+    for (let j = 0; j < values.length; j++) {
+      result.push([jsonObj[values[j]], values[j]]);
+    }
+    return result;
+  }
+  const sortedArray = arraySorter(playersAndScores);
 
-            <tbody>
-                {sortedArray.map(players => {
-                    return (
-                        <tr className="g-t-body"> 
-                            <th scope="row" className="name">{players[0]}</th>
-                            <td className="global-scores">{players[1]}</td>
-                        </tr> 
-                    )
-                })}
-            </tbody>
-        </table>
-    );
+  return (
+    <table className="g-table">
+      <thead>
+        <tr className="header">
+          <th className="g-header" scope="col">Name</th>
+          <th className="g-header" scope="col">Score</th>
+        </tr>
+      </thead>
+
+      <tbody>
+				{sortedArray.map((players, index) => {
+					return (
+						<tr key={index} className="g-t-body">
+							<th scope="row" className="name">
+								{players[0]}
+							</th>
+							<td className="global-scores">{players[1]}</td>
+						</tr>
+					);
+				})}
+      </tbody>
+    </table>
+  );
 }
 
 export default GlobalHighScores;
--- src/HighScoreTable.js
+++ src/HighScoreTable.js
@@ -1,56 +1,67 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const HighScoreTable = (props) => {
-    const countryNames = props.country.sort((a,b)=> {
-        let a1 = a.name;
-        let b1 = b.name;
-        return a1 < b1 ? -1 : a1 > b1 ? 1 : 0;
-    });
-    const descendingOrder = () => { 
-        countryNames.map(highest => {
-            return highest.scores.sort((a, b) => {
-                if (a.s > b.s) return -1;
-                else return 1;
-            });
-        });
-    }
-    const ascendingOrder = () => {countryNames.map(highest => {
-        return highest.scores.sort((a, b) => {
-            if (a.s < b.s) return -1;
-            else return 1;
-        });
-    })}
-    const [toggled, setToggled] = useState(false);
-    const toggler = (e) => {
-        setToggled(!toggled);
-        if (e.target.className === "toggle") {
-            descendingOrder(); 
-        } else if(e.target.className !== "toggle"){
-            ascendingOrder();
-          }     
-    }
+  const countryNames = props.country.sort((a, b) => {
+	let a1 = a.name;
+	let b1 = b.name;
+	return a1 < b1 ? -1 : a1 > b1 ? 1 : 0;
+  });
 
-    return (
-        <div> 
-            <button onClick={toggler} className={toggled ? "toggle" : "toggle-button"}>Sort Scores <span>&#8597;</span></button>
-            <table className="table">
-                {countryNames.map(header => (
-                    <thead> 
-                        <caption className="country-headers">High Scores: {header.name}
-                        </caption>
-                        {header.scores.map((hero) => {
-                            return (
-                                <tr className="country-heros"> 
-                                    <th>{hero.n}</th>
-                                    <th>{hero.s}</th>
-                                </tr>
-                            );
-                        })}
-                    </thead>
-                ))}
-            </table>
-        </div>
-    );
-}
+  const descendingOrder = () => {
+	countryNames.map((highest) => {
+	  return highest.scores.sort((a, b) => {
+		if (a.s > b.s) return -1;
+		else return 1;
+	  });
+	});
+  };
+
+	const ascendingOrder = () => {
+		countryNames.map((highest) => {
+			return highest.scores.sort((a, b) => {
+				if (a.s < b.s) return -1;
+				else return 1;
+			});
+		});
+	};
+
+	const [toggled, setToggled] = useState(false);
+	const toggler = (e) => {
+		setToggled(!toggled);
+		if (e.target.className === "toggle") {
+			descendingOrder();
+		} else if (e.target.className !== "toggle") {
+			ascendingOrder();
+		}
+	};
+
+	return (
+		<div>
+			<button
+				onClick={toggler}
+				className={toggled ? "toggle" : "toggle-button"}
+			>
+				Sort Scores <span>&#8597;</span>
+			</button>
+			<table className="table">
+				{countryNames.map((header) => (
+					<thead>
+						<caption className="country-headers">
+							High Scores: {header.name}
+						</caption>
+						{header.scores.map((hero) => {
+							return (
+								<tr className="country-heros">
+									<th>{hero.n}</th>
+									<th>{hero.s}</th>
+								</tr>
+							);
+						})}
+					</thead>
+				))}
+			</table>
+		</div>
+	);
+};
 
 export default HighScoreTable;
